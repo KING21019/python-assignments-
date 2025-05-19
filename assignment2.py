761ec7429830c4f9c5ac6402ac1df629fe8874be
KING21019 <webdevs159@gmail.com>
2025-05-19 10:26:43 +0530

    Add files via upload

---
+++ assignment2.py
@@ -0,0 +1,9 @@
+# implementation of linear equation
+a = 10
+b = 6
+
+if a != 0:
+    x = -b / a
+    print(f"Solution of equation {a}x + {b} = 0 is x = {x}")
+else:
+    print("No solution or infinite solutions")
